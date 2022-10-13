d8d13e73cdea8713157b3da5015ae479a6719cc5
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-10-13 12:12:08 -0700

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var modelX = new Car("Tesla", "X", 2018);
+            var model3 = new Car("Tesla", "3", 2018);
+            var carList = new List<Car>() { modelX, model3};
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
         }
     }
 }
